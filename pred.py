dd6908c9b05352cd267d3dc4acf3aa51c21a0e12
mejdi <mejdo.houm@gmail.com>
2022-09-19 09:05:02 +0200

    ajout de numpy

--- pred.py
+++ pred.py
@@ -8,3 +8,5 @@ iris_df = pd.read_csv("../iris.csv")
 y = iris_df['class']
 X = iris_df.drop(labels='class', axis=1)
 train_X_iris, test_X_iris, train_y_iris, test_y_iris =  train_test_split(X, y, train_size=0.8, test_size=0.2, random_state=42)
+
+import numpy as np
